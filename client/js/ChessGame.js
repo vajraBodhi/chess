0a2430509f8fbc74f1908b8942356e91c9ccc8d7
zezh7630 <zli@esri.com>
2016-11-07 19:22:29 +0800

    convert to es6

--- client/js/ChessGame.js
+++ client/js/ChessGame.js
@@ -1,5 +1,6 @@
 "use strict";
-import { loadedImgs } from './config';
+import { loadedImgs, chessSet, canvas, gap } from './config';
+import { Render } from './Render';
 
 
 let instance = null;
@@ -32,6 +33,91 @@ export class Game {
             imgs++;
         }
 
-        let et =
+        // let et = setInterval(() => {
+
+        // })
+    }
+
+    getCampCount(camp) {
+        let count = 0;
+        for (let p in chessSet) {
+            let chess = chessSet[p];
+            if (camp === chess && chess.type) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    checkVod() {
+        let redCount = this.getCampCount('a');
+        let greenCount = this.getCampCount('b');
+
+        if (redCount === 1 && greenCount === 1) {
+            this.increaseCount();
+        }
+
+        if (redCount === 0 && greenCount === 0) {
+            return 0;
+        } else if (greenCount === 0) {
+            return 1;
+        } else if (redCount === 0) {
+            return -1;
+        } else if (this.getCampCount() >= 10) {
+            return 0;
+        }
+    }
+
+    drawing() {
+        if (canvas.getContext) {
+            let context = canvas.getContext('2d');
+            Render.drawChessBoard(canvas, context); // 棋盘
+            Render.drawAllChess(context); // 32个棋子
+
+            canvas.addEventListener('mousedown', this.clickChess.bind(this), false);
+            canvas.oncontextmenu = () => {
+                return false;
+            }
+        }
+    }
+
+    clickChess(evt) {
+        if (window.turn) {
+            evt = evt || window.event;
+            evt.preventDefault();
+            let x = evt.offsetX || evt.layerX;
+            let y = evt.offsetY || evt.layerY;
+
+            //只使用下半盘
+            if (x > gap && y > 6 * gap) {
+                let row = Number.parseInt((evt.target.height - y) / gap, 10);
+                let col = Number.parseInt(x / gap, 10);
+
+                console.log(`${row} , ${col}`);
+                let context = canvas.getContext('2d');
+                window.clickCamp = camp;
+                Render.drawChess(context, row, col, evt.button, evt.button == 0 ? true : false);
+            }
+        }
+        evt.stopPropagation();
+    }
+
+    showBulletin(status) {
+        let content = '';
+        if (status === 0) {
+            content = '和棋';
+        } else if (status > 0) {
+            content = '红旗胜';
+        } else if (status < 0) {
+            content = '绿棋胜';
+        } else {
+            return;
+        }
+
+        let bulletin = document.getElementById('p');
+        bulletin.innerHTML = "<b>比赛结束</b><span>" + content + "</span>";
+        let mainBox = document.getElementsByClassName('mainBoxCenter')[0];
+        mainBox.appendChild(bulletin);
+        window.turn = false; // 比赛停止
     }
 }
--- client/js/config.js
+++ client/js/config.js
@@ -6,11 +6,11 @@ export let selectedRC = ''; // 以选中的棋子所在行列：1_2
 export let movedRC = ''; //  吃子或兑子所发生行列: 1_3
 export const loadedImgs = {}; // 已加载图片
 export let canvas = null;
-export let turn = false; // 保证交替执行
+window.turn = false; // export let turn = false; // 保证交替执行
 export let userName = 'lzz';
 export let rivalName = ''; // 对手
 export let camp = ''; // a 红, b 绿
-export let clickCamp = ''; // 当前执行方
+window.clickCamp = ''; // export let clickCamp = ''; // 当前执行方
 export let Socket = null;
 export let app = null;
 //var chaseCount = 0; // 如果双方各剩一子，没有办法吃掉对方，相互追逐次数超过5次系统判定为和棋
